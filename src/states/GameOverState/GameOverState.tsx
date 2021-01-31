15acbfa4849d7f9af4b39518097b4683ac4190bc
Ivan Shcherbakov <isprogfun@gmail.com>
2021-01-31 21:48:17 +0100

    fix: Changed wording

--- src/states/GameOverState/GameOverState.tsx
+++ src/states/GameOverState/GameOverState.tsx
@@ -3,7 +3,9 @@ import { GAME_STATE } from "../../components/App/types";
 export default function GameOverState(props: GameOverStateProps) {
   return (
     <div className="flex flex-col items-center justify-center">
-      <h2 className="mb-4">You have lost. Do you want to try again?</h2>
+      <h2 className="mb-4">
+        You've been eaten by Domokun. Do you want to try again?
+      </h2>
       <button onClick={() => props.onSetGameState(GAME_STATE.Initial)}>
         Start again
       </button>
